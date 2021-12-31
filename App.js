242d25c6a7612f650b11dbd320f5bd558a120bcf
Mtn Drew <drewc3@gmail.com>
2021-12-31 09:00:35 -0700

    before state

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.myMainScreen}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "flex-end",
+        }}
+      >
+        <TextInput placeholder="Course Goal!" style={styles.myInputContainer} />
+        <Button title="Add" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  myMainScreen: {
+    padding: 50,
+  },
+  myInputContainer: {
+    borderColor: "red",
+    borderWidth: 1,
+    marginBottom: 5,
+    width: "80%",
+    paddingLeft: 10,
   },
 });
